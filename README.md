e0b992da5a5a72f7cc32776ab75a41e8f07cb750
Hiroji <joegilbertdev@outlook.com>
2023-11-12 13:24:48 +0000

    basic function definitions / initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Learning Kotling
+
+## Useful commands
+
+- compile: `kotlinc <source .kt file_path> -include-runtime -d <build_file_path>`
+- run: `java -jar <build_file_path>`
---
+++ src/hello.kt
@@ -0,0 +1,18 @@
+// main function
+fun main() {
+    println("Merp!")
+    mainArrowFunction()
+}
+
+/*
+Variable declaration: 
+val = read only
+var = read write
+*/
+val user = "Hiroji"
+
+// Arrow function definition with string interpolation
+fun mainArrowFunction() = println("Hello, $user.")
+
+
+
